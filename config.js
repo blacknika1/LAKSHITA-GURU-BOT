cf1509f99a98171c490c8ac0671575994528a564
blacknika1 <138369394+blacknika1@users.noreply.github.com>
2023-07-02 16:31:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94775095950', 'SITHUM', true],
+  ['94775095950', 'BLACK NIKA', true], 
   [''] 
 ] //Number of owners
 
